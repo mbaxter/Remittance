eb432156ee68dcde86e18a9fd1554fcfe58cc1f1
mbaxter <meredith.a.baxter@gmail.com>
2017-09-04 14:21:38 -0400

    Fix bad copy/paste

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,9 @@
-const Remittance = artifacts.require("./Splitter.sol");
+const Remittance = artifacts.require("./Remittance.sol");
 
+// todo - debug deployment
 module.exports = function(deployer, network, accounts) {
 	if (accounts.length < 2) {
-		throw new Error("At least 3 available accounts must be available in order to deploy");
+		throw new Error("At least 2 available accounts must be available in order to deploy");
 	}
-	deployer.deploy(Remittance, accounts[1], 0xf2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2, 10);
+	deployer.deploy(Remittance, accounts[1], "0xf2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2", 10);
 };
